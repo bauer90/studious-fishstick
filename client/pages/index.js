a771552e9273f0ca13db805f4c27ca390c64a8c8
erhan <erhan@DESKTOP-B23PPM6>
2025-03-02 02:45:36 -0800

    show example usage of getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,13 @@
-export default () => {
+const LandingPage = ({ color }) => {
+    console.log("i'm in the component", color);
     return <h1>Landing page</h1>
-}
+} // color=red loaded to this component. 
 
+LandingPage.getInitialProps = () => {
+    console.log("I'm on the server");
+    return { color: 'red' };
+};// getInitialProps runs on server side and feeds the color=red into the LandingPage component.
+// this is how to typically load data during the server-side rendering phase.
+// e.g. to fetch signed-in status.
+
+export default LandingPage;
