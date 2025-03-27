19fe5f28ac014cd7b2a679bdb2e1f4360f0889b1
erhan <erhan.hu@gmail.com>
2025-03-27 02:28:57 -0700

    manual ack

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,7 +10,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
-    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true); // allows us to some operations (eg saving to db) before sending ack/
+    // if some error happens it will re-deliver if there is no ack
+
+    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group', options);
     // the purpose of the queue group is when multiple listener.ts start, 
     // the message will only go to one of them.
 
@@ -20,5 +24,7 @@ stan.on('connect', () => {
         if (typeof data === 'string') {
             console.log(`Received event #${msg.getSequence()} with data: ${data}`);
         }
+
+        msg.ack(); // manually ack the msg. so publisher won't deliver again.
     });
 });
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -8,6 +8,7 @@ const stan = nats.connect('ticketing', 'abc', { //create a stan client and conne
 // to make this work,
 // find nats-streaming pod name by `kubectl get pods`
 // then setup port-forward `kubectl port-forward [name] 4222:4222`
+// this will forward localhost:4222 to 4222 on the nat-streaming pod
 
 stan.on('connect', () => {
     console.log('Publisher connected to NATS');
